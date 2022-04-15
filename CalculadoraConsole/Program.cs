5ef72d53535d9d1e0372be24b4430dd7821978d3
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2022-04-15 19:00:52 -0300

    Inclusão do menu e da operação soma.

--- CalculadoraConsole/Program.cs
+++ CalculadoraConsole/Program.cs
@@ -7,7 +7,45 @@ using System.Threading.Tasks;
 namespace CalculadoraConsole {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello Word");
+            Console.Clear();
+
+            menu();
+
+            //Pause do console no final da aplicação... Fica aguardando o usuario apertar qualquer tecla para fecharo console.
+            Console.ReadKey();
+        }
+
+        static void menu() {
+            Console.WriteLine("Calculadora\n" +
+                "Escola uma das operações abaixo.\n" +
+                "1 - Soma\n" +
+                "2 - Subtração\n" +
+                "3 - Multiplicação\n" +
+                "4 - Divisão");
+
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch (escolha) {
+                case 1:
+                    soma();
+                    break;
+            }
+        }
+
+        static (float, float) dadosDeEntrada() {
+            Console.Write("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            return (v1, v2);
+        }
+
+        static void soma() {
+            
+            var result = dadosDeEntrada();
+            Console.Write("O Resultado da Soma é: " + (result.Item1 + result.Item2));
         }
     }
 }
